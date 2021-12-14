0a1b3969d8812f748588540b8638ef384bd04f8b
Daniel Mach <daniel.mach@suse.com>
2021-12-14 15:31:31 +0100

    fixup! Replace osc-wrapper.py with entry_points.

---
+++ osc-wrapper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+from osc.babysitter import main
+
+
+if __name__ == "__main__":
+    main()
--- osc/babysitter.py
+++ osc/babysitter.py
@@ -199,23 +199,38 @@ def run(prg, argv=None):
 
 
 def main():
-    # avoid buffering output on pipes (bnc#930137) Basically,
-    # a "print('foo')" call is translated to a corresponding
-    # fwrite call that writes to the stdout stream (cf.
-    # string_print (Objects/stringobject.c) and builtin_print
-    # (Python/bltinmodule.c)); If no pipe is used, stdout is
-    # a tty/refers to a terminal => the stream is line buffered
-    # (see _IO_file_doallocate (libio/filedoalloc.c)). If a pipe
-    # is used, stdout does not refer to a terminal anymore => the
-    # stream is fully buffered by default (see
-    # _IO_file_doallocate). The following fdopen call makes
-    # stdout line buffered again (at least on systems that
-    # support setvbuf - if setvbuf is not supported, the stream
-    # remains fully buffered (see PyFile_SetBufSize
-    # (Objects/fileobject.c))).
+    try:
+    # this is a hack to make osc work as expected with utf-8 characters,
+    # no matter how site.py is set...
+        reload(sys)
+        loc = locale.getpreferredencoding()
+        if not loc:
+            loc = sys.getpreferredencoding()
+        sys.setdefaultencoding(loc)
+        del sys.setdefaultencoding
+    except NameError:
+        #reload, neither setdefaultencoding are in python3
+        pass
+
+    # avoid buffering output on pipes (bnc#930137)
+    # Note: the following only applies to python2
+    # Basically, a "print('foo')" call is translated to a corresponding
+    # fwrite call that writes to the stdout stream (cf. string_print
+    # (Objects/stringobject.c) and builtin_print (Python/bltinmodule.c));
+    # If no pipe is used, stdout is a tty/refers to a terminal =>
+    # the stream is line buffered (see _IO_file_doallocate (libio/filedoalloc.c)).
+    # If a pipe is used, stdout does not refer to a terminal anymore =>
+    # the stream is fully buffered by default (see _IO_file_doallocate).
+    # The following fdopen call makes stdout line buffered again (at least on
+    # systems that support setvbuf - if setvbuf is not supported, the stream
+    # remains fully buffered (see PyFile_SetBufSize (Objects/fileobject.c))).
     if not os.isatty(sys.stdout.fileno()):
         sys.stdout = os.fdopen(sys.stdout.fileno(), sys.stdout.mode, 1)
 
+    if not os.isatty(sys.stderr.fileno()):
+        sys.stderr = os.fdopen(sys.stderr.fileno(), sys.stderr.mode, 1)
+
     sys.exit(run(commandline.Osc()))
+
 
 # vim: sw=4 et
